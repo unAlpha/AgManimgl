0cb72df879f205199e336cc6128ae2c2aa2ec999
unAlpha <pagzhong@gmail.com>
2021-09-21 00:43:46 +0800

    更换成新的下载内容

---
+++ _codes_3b1b/manim_imports_ext.py
@@ -0,0 +1,11 @@
+from manimlib import *
+
+from _codes_3b1b.custom.banner import *
+from _codes_3b1b.custom.characters.pi_creature import *
+from _codes_3b1b.custom.characters.pi_creature_animations import *
+from _codes_3b1b.custom.characters.pi_creature_scene import *
+from _codes_3b1b.custom.deprecated import *
+from _codes_3b1b.custom.end_screen import *
+from _codes_3b1b.custom.filler import *
+from _codes_3b1b.custom.logo import *
+from _codes_3b1b.custom.opening_quote import *
